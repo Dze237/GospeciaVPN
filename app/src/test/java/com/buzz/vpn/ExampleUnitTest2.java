1471ac1193e02303db702dd129d5f8bf17e299c6
dze237 <dzetfabrice@gmail.com>
2024-01-09 20:12:20 -0700

    adding new test case 2

---
+++ app/src/test/java/com/buzz/vpn/ExampleUnitTest2.java
@@ -0,0 +1,14 @@
+package com.buzz.vpn;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test multiplication logic.
+ */
+public class ExampleUnitTest2 {
+    @Test
+    public void multiplication_isCorrect() {
+        assertEquals("Multiplication failed", 12, 3 * 4);
+    }
+}
