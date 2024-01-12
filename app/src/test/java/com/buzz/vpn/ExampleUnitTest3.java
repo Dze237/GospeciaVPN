c660877040b1c36b466f31a754a95bc69275e7ce
dze237 <dzetfabrice@gmail.com>
2024-01-11 20:12:20 -0700

    adding new test case 3

---
+++ app/src/test/java/com/buzz/vpn/ExampleUnitTest3.java
@@ -0,0 +1,14 @@
+package com.buzz.vpn;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test division logic.
+ */
+public class ExampleUnitTest3 {
+    @Test
+    public void division_isCorrect() {
+        assertEquals("Division failed", 5, 10 / 2);
+    }
+}
