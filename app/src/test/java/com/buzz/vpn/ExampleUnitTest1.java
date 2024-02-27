171f597fe0812950aec6be6703cbe4c5f2303ef8
dze237 <dzetfabrice@gmail.com>
2024-02-26 20:12:20 -0700

    adding new test case 1

---
+++ app/src/test/java/com/buzz/vpn/ExampleUnitTest1.java
@@ -0,0 +1,14 @@
+package com.buzz.vpn;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test subtraction logic.
+ */
+public class ExampleUnitTest1 {
+    @Test
+    public void subtraction_isCorrect() {
+        assertEquals("Subtraction failed", 0, 2 - 2);
+    }
+}
