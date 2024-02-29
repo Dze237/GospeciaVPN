b27ff418560de47b1120c9371b4c49ae22437167
dze237 <dzetfabrice@gmail.com>
2024-02-28 20:12:20 -0700

    adding new test case 5

---
+++ app/src/test/java/com/buzz/vpn/ExampleUnitTest5.java
@@ -0,0 +1,14 @@
+package com.buzz.vpn;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test a boolean condition.
+ */
+public class ExampleUnitTest5 {
+    @Test
+    public void booleanCondition_isTrue() {
+        assertTrue("Boolean condition failed", 7 > 3);
+    }
+}
