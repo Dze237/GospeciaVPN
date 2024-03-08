59b26129468456e91f82c7731a28e3043d831cfd
dze237 <dzetfabrice@gmail.com>
2024-03-07 20:12:20 -0700

    adding new test case 6

---
+++ app/src/test/java/com/buzz/vpn/ExampleUnitTest6.java
@@ -0,0 +1,15 @@
+package com.buzz.vpn;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test that a value is null.
+ */
+public class ExampleUnitTest6 {
+    @Test
+    public void nullCheck_isCorrect() {
+        Object obj = null;
+        assertNull("Expected null", obj);
+    }
+}
